843ba1684e4aa2c3caf39b50bae82b440b4839a6
Replit user <>
2023-07-01 01:07:59 +0000

    chore: Getting the username post param

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,8 +10,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Exercise Tracker Microservice
+// requires body-parser to get the POST parameters
+const encodedDataHandler = bodyParser.urlencoded({extended: false});
+app.use(encodedDataHandler);
 
+const usersPath = '/api/users';
+const getUsername = (req, res, next) => {
+  console.log(`username: ${req.body.username}`);
 
+  next();
+};
+
+app.post(usersPath, getUsername);
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
